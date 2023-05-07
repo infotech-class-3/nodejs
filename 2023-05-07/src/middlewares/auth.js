f1ee2f6288cb1eaede7405b2705e2166d7905ea0
murat <merdo.net@gmail.com>
2023-05-07 18:22:58 +0200

    add full node backend implementation

---
+++ 2023-05-07/src/middlewares/auth.js
@@ -0,0 +1,20 @@
+// middleware
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  const headerAuth = req.headers["authorization"];
+  if (!headerAuth) {
+    return res.status(403).json({ message: "No token data." });
+  }
+  const token = headerAuth.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, data) => {
+    if (err) {
+      return res.status(403).json({ message: "Unauthorized user..", err });
+    }
+
+    next();
+  });
+};
+
+module.exports = auth;
